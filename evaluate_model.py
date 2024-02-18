7c95c4f706c885075da9d471cd7aaf9321f131d0
sebastianbrzustowicz <Se.Brzustowicz@gmail.com>
2024-02-18 23:32:28 +0100

    change for more suitable word

--- evaluate_model.py
+++ evaluate_model.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 def plot_data(data, indices_of_max_values, y_test_val, y_hat_val, num_rows_to_plot=5):
     print("Printing 5 most incorrect data test cases")
-    print(f"1. True value: {y_test_val[indices_of_max_values[0]]}, neural network guess: {y_hat_val[indices_of_max_values[0]]}, index: {indices_of_max_values[0]+1}")
-    print(f"2. True value: {y_test_val[indices_of_max_values[1]]}, neural network guess: {y_hat_val[indices_of_max_values[1]]}, index: {indices_of_max_values[1]+1}")
-    print(f"3. True value: {y_test_val[indices_of_max_values[2]]}, neural network guess: {y_hat_val[indices_of_max_values[2]]}, index: {indices_of_max_values[2]+1}")
-    print(f"4. True value: {y_test_val[indices_of_max_values[3]]}, neural network guess: {y_hat_val[indices_of_max_values[3]]}, index: {indices_of_max_values[3]+1}")
-    print(f"5. True value: {y_test_val[indices_of_max_values[4]]}, neural network guess: {y_hat_val[indices_of_max_values[4]]}, index: {indices_of_max_values[4]+1}")
+    print(f"1. Real value: {y_test_val[indices_of_max_values[0]]}, neural network guess: {y_hat_val[indices_of_max_values[0]]}, index: {indices_of_max_values[0]+1}")
+    print(f"2. Real value: {y_test_val[indices_of_max_values[1]]}, neural network guess: {y_hat_val[indices_of_max_values[1]]}, index: {indices_of_max_values[1]+1}")
+    print(f"3. Real value: {y_test_val[indices_of_max_values[2]]}, neural network guess: {y_hat_val[indices_of_max_values[2]]}, index: {indices_of_max_values[2]+1}")
+    print(f"4. Real value: {y_test_val[indices_of_max_values[3]]}, neural network guess: {y_hat_val[indices_of_max_values[3]]}, index: {indices_of_max_values[3]+1}")
+    print(f"5. Real value: {y_test_val[indices_of_max_values[4]]}, neural network guess: {y_hat_val[indices_of_max_values[4]]}, index: {indices_of_max_values[4]+1}")
 
     plt.figure(figsize=(10, 6))
 
@@ -21,12 +21,12 @@ def plot_data(data, indices_of_max_values, y_test_val, y_hat_val, num_rows_to_pl
     plt.show()
 
 def single_plot(data, index, y_test_val, y_hat_val, num_rows_to_plot=10):
-    print(f"Most incorrect data test case\nTrue value: {y_test_val}, neural network guess: {y_hat_val}, error: {abs(y_test_val-y_hat_val)}")
+    print(f"Most incorrect data test case\nReal value: {y_test_val}, neural network guess: {y_hat_val}, error: {abs(y_test_val-y_hat_val)}")
     plt.figure(figsize=(10, 6))
 
     plt.plot(data[index, :], label=f'Worst case')
 
-    plt.title(f'Most incorrect data test case\nTrue value: {y_test_val}, neural network guess: {y_hat_val}, error: {abs(y_test_val-y_hat_val)}, index: {index+1}')
+    plt.title(f'Most incorrect data test case\nReal value: {y_test_val}, neural network guess: {y_hat_val}, error: {abs(y_test_val-y_hat_val)}, index: {index+1}')
     plt.xlabel('Index')
     plt.ylabel('Value')
     plt.legend()
