675488d7dc6bbf44b85f5a47c097705304fecb98
sebastianbrzustowicz <Se.Brzustowicz@gmail.com>
2024-02-19 22:59:51 +0100

    adding pwd to docker source

--- startservice.sh
+++ startservice.sh
@@ -5,4 +5,4 @@ docker pull tensorflow/serving
 docker stop collision-detection-ai
 docker rm collision-detection-ai
 
-docker run -p 8501:8501 -d --name=collision-detection-ai --mount type=bind,source="your_path_to_model\tfmodel",target=/models/tfmodel/1 -e MODEL_NAME=tfmodel -t tensorflow/serving
+docker run -p 8501:8501 -d --name=collision-detection-ai --mount type=bind,source="$(pwd)"/tfmodel,target=/models/tfmodel/1 -e MODEL_NAME=tfmodel -t tensorflow/serving
